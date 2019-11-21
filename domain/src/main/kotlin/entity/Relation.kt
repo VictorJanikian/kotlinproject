4cc22d23496b31f464774b3e7426ad2f3f75727e
Victor Janikian <vc.janikian@hotmail.com>
2019-11-20 23:34:59 -0300

    first commit

---
+++ domain/src/main/kotlin/entity/Relation.kt
@@ -0,0 +1,3 @@
+package entity
+
+data class Relation (val primaryCorporation : Corporation, val secondCorporation : Corporation)
--- domain/src/main/kotlin/service/CorporationService.kt
+++ domain/src/main/kotlin/service/CorporationService.kt
@@ -4,12 +4,26 @@ import entity.Corporation
 import repository.CorporationRepository
 
 class CorporationService(val repository: CorporationRepository) {
-    fun createCorporation(value: Corporation) {
 
+    fun createCorporation(value: Corporation) {
+        repository.save(value)
     }
 
     fun searchCorporationById(value: String) {
+        repository.fetchById(value)
+    }
 
+    fun searchCorporationByDocument(value: String){
+        repository.fetchByDocuments(value)
+    }
+
+    fun updateCorporation(value: Corporation) {
+        val corporation = searchCorporationById(value.id)
+        if (corporation != null) updateCorporation(value) else print("Can't perform this operation")
+    }
+
+    fun removeCorporation(value: String){
+        removeCorporation(value)
     }
 
 }
